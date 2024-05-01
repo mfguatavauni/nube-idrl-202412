f035aff1b6f834d0636d4176178b586cc100d6b4
Sandra Guevara <sandraguevara@Sandra-2.local>
2024-05-01 16:32:01 -0500

    Fix redis

--- IdrlNube202412/app/celery_config.py
+++ IdrlNube202412/app/celery_config.py
@@ -3,8 +3,8 @@ from celery import Celery
 def make_celery():
     return Celery(
         'app',
-        backend='redis://34.72.18.166:6379/0',
-        broker='redis://34.72.18.166:6379/0'
+        backend='redis://34.121.134.171:6379/0',
+        broker='redis://34.121.134.171:6379/0'
     )
 
 celery = make_celery()
